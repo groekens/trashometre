f74c320c8e303c64ad901e17714c6268252d269a
groekens <67225369+groekens@users.noreply.github.com>
2026-05-12 06:18:57 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 //   - Bypass for Firebase (let Firebase handle its own caching)
 // ─────────────────────────────────────────────────────────────────────────────
 
-const VERSION = 'v2.7.0';
+const VERSION = 'v2.7.2';
 const APP_CACHE    = `trashometre-app-${VERSION}`;
 const ASSETS_CACHE = `trashometre-assets-${VERSION}`;
 
